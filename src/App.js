eee6d541633314427a6dd51fd25e476caca40ad1
kelshardy <14024@coderacademy.edu.au>
2023-09-21 20:16:58 +1000

    feat: export business card parent and add into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BusinessCardParent from './BusinessCardParent';
 
 function App() {
   return (
     <div className="App">
-      
+      <BusinessCardParent />
     </div>
   );
 }
